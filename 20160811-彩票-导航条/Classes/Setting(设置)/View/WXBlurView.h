0d84658d92f60cfbd800fd43cd7ba1509bdb4086
WangSanPing <wangxu_1987@126.com>
2016-09-21 17:22:28 +0800

    添加DRNRealTimeBlurView(高斯模糊)框架和MBProgressHUD(提示框)框架

---
+++ 20160811-彩票-导航条/Classes/Setting(设置)/View/WXBlurView.h
@@ -0,0 +1,13 @@
+//
+//  WXBlurView.h
+//  20160811-彩票
+//
+//  Created by 王旭 on 2016/9/21.
+//  Copyright © 2016年 wangxu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXBlurView : NSObject
+
+@end
---
+++ 20160811-彩票-导航条/Classes/Setting(设置)/View/WXBlurView.m
@@ -0,0 +1,13 @@
+//
+//  WXBlurView.m
+//  20160811-彩票
+//
+//  Created by 王旭 on 2016/9/21.
+//  Copyright © 2016年 wangxu. All rights reserved.
+//
+
+#import "WXBlurView.h"
+
+@implementation WXBlurView
+
+@end
